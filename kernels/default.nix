277f42ecb727228f2155a3a76b1fc65f95e61fc6
Kristoffer Søholm <k.soeholm@gmail.com>
2020-07-01 13:40:08 +0200

    Rename rust to rustWith, add to tests

--- kernels/default.nix
+++ kernels/default.nix
@@ -13,5 +13,5 @@
   xeusCling = callPackage ./xeus-cling;
   iJavascript = callPackage ./ijavascript;
   gophernotes = callPackage ./gophernotes;
-  rust = callPackage ./rust;
+  rustWith = callPackage ./rust;
 }
